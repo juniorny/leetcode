45b79f93da0e7ca6322cf5e2b760a7ce34bc559b
juniorny <35912735+juniorny@users.noreply.github.com>
2019-09-06 20:54:04 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -13,10 +13,26 @@ int main()
     int num2 = 0;
     int count = 0;
     unsigned int i = 0;
+    int sign = 1;
+
+    while (s[i] == ' ')
+        i++;
+
+    if (s[i] == '-' || s[i] == '+')
+        sign = (s[i++] == '+') ? 1 : -1;
+
     while (s[i] != '.' && s[i] != '\0')
     {
-        num = 10 * num + (s[i] - '0');
-        i++;
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            num = 10 * num + (s[i] - '0');
+            i++;
+        }
+        else
+        {
+            cout << num * sign << endl;
+            return 0;
+        }
     }
 
     if (s[i] == '.')
@@ -24,16 +40,18 @@ int main()
 
     while (s[i] != '\0')
     {
-        num2 = 10 * num2 + (s[i] - '0');
-        count++;
-        i++;
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            num2 = 10 * num2 + (s[i] - '0');
+            count++;
+            i++;
+        }
+        else
+        {
+            break;
+        }
     }
 
-//    cout << num << ' ' << num2 << endl;
-
-    float res = num + num2 / pow(10, count);
-
-    cout << res << endl;
-
+    cout << (num + (float)num2 / pow(10, count)) * sign << endl;
     return 0;
 }
